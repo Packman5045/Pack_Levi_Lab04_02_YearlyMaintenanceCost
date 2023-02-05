b539c6dbc9536e5c47ef777e65dce1e96774f4f1
Packman5045 <packman.lp@gmail.com>
2023-02-04 23:54:17 -0500

    Initial commit_Lab04_02

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+        public static void main(String[] args) {
+
+            int summer = 123;
+            int spring = 213;
+            int winter = 392;
+            int fall = 59;
+            int total = summer + spring + winter + fall;
+
+
+            System.out.println("The total for the yearly maintenance costs are" + " " + total);
+
+        }
+
+    }
